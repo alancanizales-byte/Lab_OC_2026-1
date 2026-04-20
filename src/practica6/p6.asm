6a43d379a6725540040596b948aa275d26f41930
alancanizales-byte <alan.canizales@uabc.edu.mx>
2026-04-20 13:45:52 -0700

    PRACTICA 6 ACTUALIZADA

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -1,84 +1,167 @@
-%include "../../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
-								    ; que se encuentran en la biblioteca libpc_io.a
+%include "../../lib/pc_io.inc"
 
-section	.text
-	global _start       ;referencia para inicio de programa
-	
-_start:                   
-    call getch
-    call itoa
-    mov edx,ncad
+section .text
+    global _start
+
+_start:
+    mov edx, msg        ; mensaje para pedir cadena
     call puts
 
-    mov bx,word[len]
-    mov edx,cad
+    mov ax, 64          ; capturar cadena
+    mov edx, cad
     call capturar
-    mov al,[nlin]
+
+    mov al, 0x0A        ; salto de linea
     call putchar
+
+    mov edx, original       ; mostrar cadena original
+    call puts
+    mov edx, cad
     call puts
 
-    mov al,[nlin]
+    mov edx, cad            ; convertir a mayusculas
+    call mayusculas
+
+    mov edx, mayus          ; mostrar cadena en mayusculas
+    call puts
+    mov edx, cad
+    call puts
+
+    mov edx, cad            ; convertir a minusculas
+    call minusculas
+
+    mov edx, minus          ; mostrar cadena en minusculas
+    call puts
+    mov edx, cad
+    call puts
+
+    mov al, 0x0A            ; salto de linea
     call putchar
+
+    ; Salida del programa
+    mov eax, 1
+    int 0x80
+
+capturar:
+    push ax
+    push bx
+    push cx
+    push dx
+
+    mov cx, ax
+    dec cx 
+    xor bx, bx             
+
+.ciclo:
+    call getch
+
+    cmp al, 0x0A        ; ¿es enter?
+    je .fin
+
+    cmp al, 0x08        ; ¿es backspace?
+    je .borrar
+
+    cmp al, 0x7F         ; ¿es delete?
+    je .borrar
+
+    cmp cx, 0             ; ¿es espacio?
+    je .ciclo
+
+    call putchar        ; mostrar caracter
+    mov [edx], al
+
+    inc edx              ; avanzar puntero
+    inc bx               ; incrementar longitud
+    dec cx               ; reducir espacio disponible
+    jmp .ciclo
+
+.borrar:
+    cmp bx, 0
+    je .ciclo
+
+    dec edx
+    dec bx
+    inc cx
+
+    mov al, 0x08
+    call putchar
+    mov al, ' '
+    call putchar
+    mov al, 0x08
     call putchar
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
+    jmp .ciclo
 
-    capturar:
-        push edx
-        push cx
-        mov cx,bx
-        dec cx
+.fin:
+    mov byte [edx], 0   
 
-    .ciclo: 
-        call getch
-        cmp al,127
-        jne .guardar
-        call borrar
-        jmp .ciclo
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
 
-       .guardar:
-        call putchar
-        mov [edx],al
-        cmp al,0xa
-        je .salir
-        inc edx
-        loop .ciclo
+mayusculas:
+    push ax
+    push dx
 
-        .salir:
-        mov byte[edx],0
-        pop cx
-        pop edx
-        ret
+.recorrer:
+    mov al, [edx]
 
-    borrar:
-        push ax 
-        mov al,0x8
-        call putchar    
-        mov al,' '
-        call putchar
-        mov al,0x8
-        call putchar   
-        pop ax
-        ret 
+    cmp al, 0
+    je .fin
 
-    itoa:
-        push bx
-        mov bl,100
-        mov ah,0
-        div bl
-        mov bx,ax
-        add al,'0'
-        call putchar
-        mov al,ah
-        add al,'0'
-        call putchar
+    cmp al, 'a'
+    jb .sig
 
-        pop bx
-        ret
+    cmp al, 'z'
+    ja .sig
 
-section	.data
-    ncad db 0xa,'Cadena: ',0
-    nlin db 0xa
-    len db 64
-    cad	times 64 db 0
+    sub al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop dx
+    pop ax
+    ret
+
+minusculas:
+    push ax
+    push dx
+
+.recorrer:
+    mov al, [edx]
+
+    cmp al, 0
+    je .fin
+
+    cmp al, 'A'
+    jb .sig
+
+    cmp al, 'Z'
+    ja .sig
+
+    add al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop dx
+    pop ax
+    ret
+
+section .data
+    msg db "Ingresa una cadena: ",0
+    original db 0x0A,"Original: ",0
+    mayus db 0x0A,"Mayusculas: ",0
+    minus db 0x0A,"Minusculas: ",0
+
+section .bss
+    cad resb 64
