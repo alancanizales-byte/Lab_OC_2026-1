802ff4888fbce2745e91a25b051bf75b709394aa
alancanizales-byte <alan.canizales@uabc.edu.mx>
2026-05-12 20:42:40 +0000

    PRACTICA 9 - MAIN

---
+++ src/practica9/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+extern int maximo(int *arr, int len);
+extern int minimo(int *arr, int len);
+extern int sumatoria(int *arr, int len);
+
+int main(){
+    int arr[5];
+    int n;
+
+    printf("Cantidad de elementos: ");
+    scanf("%d", &n);
+
+    if(n < 1 || n > 5){
+        printf("Cantidad invalida");
+        return 1;
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("Elemento [%d]: ");
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Elementos del arreglo:\n");
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nMaximo: %d\n", maximo(arr, n);
+    printf("Minimo: %d", minimo(arr, n));
+    printf("Sumatoria: %d", sumatoria(arr, n));
+
+    return 0;
+}
